49e6a90cc5a8c3f1b09c046dd354c2be1ad1e8fd
Lei Zhao <lei.zhao@coupa.com>
2020-11-13 18:00:13 -0800

    add by auto checkIn

--- audioPlay/audioPlay/AudioManager.swift
+++ audioPlay/audioPlay/AudioManager.swift
@@ -7,30 +7,98 @@ import AVFoundation
 import UIKit
 import AudioToolbox
 
-class AudioManager {
+class AudioManager: NSObject {
     private var audioPlayer: AVAudioPlayer?
+    private var audioPlayerDelegate: AudioPlayerDelegate?
+    private var recorder: AVAudioRecorder?
     private let session = AVAudioSession.sharedInstance()
+    private var audioPlayerUnmutedVolume: Float?
 
     static let sharedInstance = AudioManager()
 
-    private init() {
+    private override init() {
     }
 
-    public func playAudio(url: URL, onError: ((Error) -> Void)? = nil) {
+    public func startNewAudio(
+      url: URL,
+      onFinishingPlaying: ((_ successfully: Bool) -> Void)? = nil,
+      onError: ((Error) -> Void)? = nil) {
         do {
+            if let audioPlayer = self.audioPlayer {
+                audioPlayer.stop()
+            }
+
             // we need this line to show the high volume
             try AVAudioSession.sharedInstance().setCategory(.playback)
 
             let audioPlayer = try AVAudioPlayer(contentsOf: url)
+
+            self.audioPlayerDelegate = AudioPlayerDelegate()
+            self.audioPlayerDelegate?.onFinishingPlaying = onFinishingPlaying
+            audioPlayer.delegate = self.audioPlayerDelegate
+
             self.audioPlayer = audioPlayer
+
+            if let audioPlayerUnmutedVolume = self.audioPlayerUnmutedVolume {
+                self.setAudioVolume(volume: audioPlayerUnmutedVolume)
+            } else {
+                self.audioPlayerUnmutedVolume = self.audioPlayer?.volume
+            }
+
+//            self.audioPlayer?.isMeteringEnabled = true
+
             self.audioPlayer?.play()
         } catch {
             onError?(error)
         }
     }
 
+    public func audioIsPlaying() -> Bool? {
+        self.audioPlayer?.isPlaying
+    }
+
+    public func pauseAudio() {
+        self.audioPlayer?.pause()
+    }
+
+    public func resumeAudio() {
+        self.audioPlayer?.play()
+    }
+
     public func stopAudio() {
         self.audioPlayer?.stop()
+    }
+
+    public func muteAudio() {
+        self.audioPlayer?.volume = 0
+    }
+
+    public func unmuteAudio() {
+        self.audioPlayer?.volume = audioPlayerUnmutedVolume ?? 0
+    }
+
+    public func audioIsMuted() -> Bool {
+        self.audioPlayer?.volume == 0 && self.audioPlayerUnmutedVolume != 0
+    }
+
+    public func setAudioVolume(volume: Float) {
+        self.audioPlayer?.volume = volume
+        self.audioPlayerUnmutedVolume = volume
+    }
+
+    public func getAudioDuration() -> TimeInterval {
+        self.audioPlayer?.duration ?? 0
+    }
+
+    public func setAudioPlayTime(timeInterval: TimeInterval) {
+        self.audioPlayer?.pause()
+        self.audioPlayer?.currentTime = timeInterval
+        self.audioPlayer?.play()
+    }
+
+    public func setAudioPlayerLoop(num: Int) {
+        print("...set Loop...", num)
+        self.audioPlayer?.numberOfLoops = num
     }
 
     public func playVibrateSound(onError: ((Error) -> Void)? = nil) {
@@ -42,21 +110,73 @@ class AudioManager {
         }
     }
 
+    public func getAudioInfo(url: URL) -> [String: String] {
+        var audioFileID: AudioFileID?
+        AudioFileOpenURL(url as CFURL, .readPermission, 0, &audioFileID)
+        var size: UInt32 = 0
+        AudioFileGetPropertyInfo(audioFileID!, kAudioFilePropertyInfoDictionary, &size, .none)
+
+        var dictionary: CFDictionary?
+        AudioFileGetProperty(audioFileID!, kAudioFilePropertyInfoDictionary, &size, &dictionary)
+
+
+        return dictionary as! [String: String]
+    }
+
+    // sound id can be found here: http://iphonedevwiki.net/index.php/AudioServices or https://github.com/TUNER88/iOSSystemSoundsLibrary
     public func playSystemSound(
-      url: URL? = Bundle(identifier: "com.apple.UIKit")?.url(forResource: "Tock", withExtension: "aiff"),
+      soundId: UInt32? = 1104,
       onComplete: (() -> Void)? = nil
     ) {
-        let soundId: SystemSoundID
-        AudioServicesCreateSystemSoundID(url, soundId)
-        AudioServicesAddSystemSoundCompletion(soundId, nil, nil, { id, pointer in
-            AudioServicesRemoveSystemSoundCompletion(id)
-            AudioServicesDisposeSystemSoundID(id)
-        }, nil)
+        AudioServicesPlayAlertSoundWithCompletion(soundId!, onComplete)
+    }
 
-        AudioServicesPlayAlertSoundWithCompletion(soundId, onComplete)
+    // sound id can be found here: http://iphonedevwiki.net/index.php/AudioServices or https://github.com/TUNER88/iOSSystemSoundsLibrary
+    public func playSystemAlertSound(
+      soundId: UInt32? = 1104,
+      onComplete: (() -> Void)? = nil) {
+        AudioServicesPlaySystemSoundWithCompletion(soundId!, onComplete)
     }
 }
 
+// record
+extension AudioManager {
+
+    public func startRecord(
+      saveUrl: URL = URL(fileURLWithPath: NSHomeDirectory()).appendingPathComponent("Documents/record.m4a"),
+      onError: ((Error) -> Void)? = nil,
+      AVSampleRate: Int = 44100,
+      AVFormatID: UInt32 = kAudioFormatMPEG4AAC,
+      AVNumberOfChannels: Int = 1,
+      AVEncoderAudioQuality: AVAudioQuality = AVAudioQuality.high
+    ) {
+
+        do {
+
+            try AVAudioSession.sharedInstance().setCategory(.playAndRecord)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            let settings: [String: Any] = [
+                AVSampleRateKey: AVSampleRate,
+                AVFormatIDKey: AVFormatID,
+                AVNumberOfChannelsKey: AVNumberOfChannels,
+                AVEncoderAudioQualityKey: AVEncoderAudioQuality.rawValue
+            ]
+            let recorder = try AVAudioRecorder(url: saveUrl, settings: settings)
+            self.recorder = recorder
+            recorder.record()
+        } catch {
+            onError?(error)
+        }
+    }
+
+    public func stopRecord() {
+        try? AVAudioSession.sharedInstance().setCategory(.playback)
+        self.recorder?.stop()
+    }
+}
+
+// custom type
 extension AudioManager {
     struct DeviceNotSupportError: LocalizedError {
         var errorDescription: String? {
@@ -70,6 +190,25 @@ extension AudioManager {
 
         init(description: String) {
             self._description = description
+        }
+    }
+
+    class AudioPlayerDelegate: NSObject, AVAudioPlayerDelegate {
+
+        var onFinishingPlaying: ((_ flag: Bool) -> Void)?
+
+        func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+            print("...audioPlayerDidFinishPlaying...")
+            onFinishingPlaying?(flag)
+        }
+
+        func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        }
+
+        func audioPlayerBeginInterruption(_ player: AVAudioPlayer) {
+        }
+
+        func audioPlayerEndInterruption(_ player: AVAudioPlayer, withOptions flags: Int) {
         }
     }
 }
--- audioPlay/audioPlay/ContentView.swift
+++ audioPlay/audioPlay/ContentView.swift
@@ -11,22 +11,80 @@ import SwiftUI
 struct ContentView: View {
 
     let audioManager = AudioManager.sharedInstance
+    let url = Bundle.main.url(forResource: "爸爸去哪儿.mp3", withExtension: nil)
+    @State private var isPlaying: Bool?
+    @State(initialValue: false) private var isMute
+    @State(initialValue: 0.5) private var volume
+    @State(initialValue: 1) private var numOfLoop
 
     var body: some View {
-        VStack(spacing: 20) {
-            Button("震动") {
-                audioManager.playVibrateSound { error in
-                    print("...error...\(error.localizedDescription)")
+        VStack {
+            HStack {
+                Button("Star") {
+                    self.numOfLoop = 1
+                    audioManager.startNewAudio(url: url!, onFinishingPlaying: { b in
+                        print("....finish....")
+                        self.numOfLoop -= 1
+                    })
+                    self.isPlaying = true
+                }
+                if let isPlaying = self.isPlaying {
+                    if isPlaying {
+                        Button("Pause") {
+                            audioManager.pauseAudio()
+                            self.isPlaying = false
+                        }
+                    } else {
+                        Button("Resume") {
+                            audioManager.resumeAudio()
+                            self.isPlaying = true
+                            audioManager.setAudioPlayTime(timeInterval: 270)
+                        }
+                    }
+                }
+                Button("Stop") {
+                    audioManager.stopAudio()
+                    self.isPlaying = nil
                 }
             }
 
-            Button("系统声音") {
-                audioManager.playSystemSound { print("...finish system sound...") }
+            HStack {
+                Button("Mute") {
+                    self.isMute.toggle()
+                }
+                Toggle("", isOn: $isMute)
+                  .labelsHidden()
+                  .onChange(of: isMute) {
+                      $0 ? audioManager.muteAudio() : audioManager.unmuteAudio()
+                  }
             }
 
-            Button("提示音") {
+            HStack {
+                Text("Vol")
+                Slider(value: $volume, in: 0...1, step: 0.1)
+                  .onChange(of: volume) {
+                      audioManager.setAudioVolume(volume: Float($0))
+                  }
             }
+              .padding(
+                [.leading, .trailing], 40
+              )
+
+            HStack {
+                Text("Num of Loop: \(self.numOfLoop)")
+                Stepper("", value: $numOfLoop, step: 1).labelsHidden()
+                  .onChange(of: numOfLoop) { loop in
+                      audioManager.setAudioPlayerLoop(num: loop)
+                  }
+            }
+              .padding(
+                [.leading, .trailing], 40
+              )
+
+            Text("\(audioManager.getAudioDuration())")
         }
+          .onAppear {
+              audioManager.setAudioVolume(volume: Float(self.volume))
+          }
     }
 }
-
